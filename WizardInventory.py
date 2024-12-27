0a95745e6446656b4c1f5f0131c9b73cab8f653f
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-26 20:36:06 -0330

    Drop an item updated menu

--- WizardInventory.py
+++ WizardInventory.py
@@ -2,17 +2,30 @@ import random
 def main():
  playerinventory = ["Wooden staff", "Wizard Hat", "Cloth"]
  while True:
-    command = input("Enter a command")
-    if command == "show":
-        ShowItems(playerinventory)
-    elif command == "grab":
-        GrabItems(playerinventory)
+    DisplayMenu()
+    try:
+        command =int(input("Enter a command"))
+        if command > 4:
+            print("Enter a vaild command")
+            continue
+        if command == 1:
+            ShowItems(playerinventory)
+        elif command == 2:
+            GrabItems(playerinventory)
+        elif command == 3:
+            DropItem(playerinventory)
+        elif command == 4:
+            print("GoodBye")
+            return False
+    except ValueError:
+        print("Enter a vaild number")
+
 def DisplayMenu():
     print("COMMAND MENU")
-    print("Show - Show all items")
-    print("Grab - Grab an item")
-    print("Edit - Edit an item")
-    print("Exit - Exit the program")
+    print("1 - Show all items")
+    print("2 - Grab an item")
+    print("3 - Drop an item")
+    print("4 - Exit the program")
 
 def ShowItems(player_inventory):
    for i, items in enumerate(player_inventory):
@@ -22,9 +35,21 @@ def GrabItems(player_inventory):
  find_items = ["Potion of Invisibility", "Cool hat", "Raggedy boots", "Shabby sword"]
  ShowItems(player_inventory)
  for items in find_items:
-    if len(player_inventory) > 4:
+    if len(player_inventory) > 3:
         print("You can only hold 4 items - drop one first.")
+        break
     else:
         player_inventory.append(random.choice(find_items))
+        break
+
+def DropItem(player_inventory):
+ while True:
+    user_number = int(input("Number for item to delete: "))
+    if user_number < 1 or user_number > len(player_inventory):
+       print("Invaild number for item in inventory")
+       continue
+    else:
+        item = player_inventory.pop(user_number-1)
+        print(f"{item} was dropped from the inventory.")
         break
 main()
